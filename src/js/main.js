89520a5ab1ec404fd3197f9c77b646156949852d
Boskolife <94291571+Boskolife@users.noreply.github.com>
2025-07-04 15:06:41 +0300

    add animations

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,15 @@
+import WOW from './wow';
+
+var wow = new WOW({
+  boxClass: 'wow',
+  animateClass: 'animated',
+  offset: 1,
+  mobile: true,
+  live: true,
+  scrollContainer: null,
+  resetAnimation: true,
+});
+wow.init();
 function burgerInit() {
   const burger = document.querySelector('.header_burger');
   const headerContent = document.querySelector('.header_content_burger');
